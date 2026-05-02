23838d815c0636246ba040c023068d04185f6689
Bhavin <bhavinpadhariya0206@gmail.com>
2026-05-02 15:27:57 +0530

    added sidebar in dashboard

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,19 +1,40 @@
+"use client";
+
+import { useSidebar } from "@/context/SidebarContext";
+// import AppHeader from "@/layout/AppHeader";
+import AppSidebar from "@/layout/AppSidebar";
+import Backdrop from "@/layout/Backdrop";
 import React from "react";
+import AppHeader from "./AppHeader";
 
 export default function AdminLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <div className="min-h-screen flex">
-            <aside className="w-64 border-r p-4">
-                Sidebar
-            </aside>
+    const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
-            <main className="flex-1 p-6">
-                {children}
-            </main>
+    // Dynamic class for main content margin based on sidebar state
+    const mainContentMargin = isMobileOpen
+        ? "ml-0"
+        : isExpanded || isHovered
+            ? "lg:ml-[290px]"
+            : "lg:ml-[90px]";
+
+    return (
+        <div className="min-h-screen xl:flex">
+            {/* Sidebar and Backdrop */}
+            <AppSidebar />
+            <Backdrop />
+            {/* Main Content Area */}
+            <div
+                className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
+            >
+                {/* Header */}
+                <AppHeader />
+                {/* Page Content */}
+                <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
+            </div>
         </div>
     );
 }
